2d5fc5bc62d98e8897cae6e570f75dbd9003d12d
weijia <2112025700@qq.com>
2024-01-10 20:47:25 +0800

    feat: 完成路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,28 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: () => import("../views/index.vue"),
+    },
+    {
+      path: "/article/:id",
+      name: "article",
+      component: () => import("../views/article.vue"),
+    },
+    {
+      path: "/mine",
+      name: "mine",
+      component: () => import("../views/mine.vue"),
+    },
+    {
+      path: "/mine_collections",
+      name: "mine_collections",
+      component: () => import("../views/mine_collections.vue"),
+    },
+  ],
 });
 
 export default router;
