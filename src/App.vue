9dc9c7f64ec8f72da02e2895b3ad2fa58fee56b8
weijia <2112025700@qq.com>
2024-01-10 20:33:53 +0800

    feat: 初始化移动端项目

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <Button type="primary">主要按钮</Button>
+</template>
+
+<script setup lang="ts">
+import { Button } from "vant";
+</script>
+
+<style scoped></style>
---
+++ src/views/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="base_view index_view">
+    <div class="head">
+      <van-search placeholder="请输入搜索关键词"></van-search>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss"></style>
